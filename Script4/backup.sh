52f3ee2fa5ce8fd3ca9c4095fd5cc00d37c11da9
Shivam Jadhav <34097420+Flash-Shivam@users.noreply.github.com>
2019-12-14 01:25:29 +0530

    output informational messages Left to be corrected Rest Done

--- Script4/backup.sh
+++ Script4/backup.sh
@@ -3,37 +3,41 @@
 		cd $1 || exit -1
 		for i in *; do
 			if [ -d "$i" ]; then
-			cd ..
+			cd $4
 			cd $2
 				if ! [ -d "$i" ]; then
 				echo $i
 				mkdir $i
 				export x=$(pwd)
-				cd ..
+				cd $3
 				cd $1
 				cp -r $i $i "$x"
-				fi
-				cd ..
+				else	
+					cd "$3/$1"
+					backup $i $i "$3/$1" "$3/$2" 
+				fi 
+				cd $3
 				cd $1
+				
 			else			
-				cd ..
+				cd $4
 				cd $2
 				if ! [ -f "$i" ]; then
 				echo $i
 				touch $i
 				export x=$(pwd)
-				cd ..
+				cd $3
 				cd $1
 				cp $i $i "$x"
 				fi
-				cd ..
+				cd $3
 				cd $1
 			fi
         	done
 	}
 	export y=$(pwd)
 	echo Files copied from $1 to $2 are:
-	backup $1 $2
+	backup $1 $2 $y $y
 	cd $y
 	echo Files copied from $2 to $1 are:
-	backup $2 $1
+	backup $2 $1 $y $y
