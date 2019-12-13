293bd82208c41477ba649e901e0e56061e3d029b
Shivam Jadhav <34097420+Flash-Shivam@users.noreply.github.com>
2019-12-14 01:52:16 +0530

    Done Backup.sh

--- Script4/backup.sh
+++ Script4/backup.sh
@@ -6,12 +6,16 @@
 			cd $4
 			cd $2
 				if ! [ -d "$i" ]; then
-				echo $i
+					if ! [[ $3 -ef $4 ]]; then
+					echo -n "$1/$i" 
+					else
+					echo -n  $i
+					fi
 				mkdir $i
 				export x=$(pwd)
 				cd $3
 				cd $1
-				cp -r $i $i "$x"
+				cp -r $i $i "$x" 2>/dev/null
 				else	
 					cd "$3/$1"
 					backup $i $i "$3/$1" "$3/$2" 
@@ -23,16 +27,21 @@
 				cd $4
 				cd $2
 				if ! [ -f "$i" ]; then
-				echo $i
+					if ! [[ $3 -ef $4 ]]; then
+					echo -n "$1/$i" 
+					else
+					echo -n  $i
+					fi
 				touch $i
 				export x=$(pwd)
 				cd $3
 				cd $1
-				cp $i $i "$x"
+				cp $i $i "$x" 2>/dev/null
 				fi
 				cd $3
 				cd $1
 			fi
+			echo
         	done
 	}
 	export y=$(pwd)
